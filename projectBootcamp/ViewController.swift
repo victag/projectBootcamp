e4ed97b146d54bd731d99b0fe7b215363873e239
Vam <vamaguero@gmail.com>
2023-11-14 14:23:40 -0500

    Initial Commit

---
+++ projectBootcamp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  projectBootcamp
+//
+//  Created by NTTDATA on 14/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Lable1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        Lable1.text = "Hola mundo"
+    
+    }
+
+
+}
+
